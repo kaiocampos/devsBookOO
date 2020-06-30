0bb67cc6dc599435e552aa3c6ddc57f4f5b95f80
Kaio Campos <kaiocamposti@gmail.com>
2020-06-30 02:43:26 -0300

    inserindo metodos nas interfaces

--- models/Post.php
+++ models/Post.php
@@ -8,5 +8,7 @@ class Post{
 }
 interface PostDAO{
     public function insert(Post $p);
+    public function getUserFeed($id_user);
     public function getHomeFeed($id_user);
+    public function getPhotosFrom($id_user);
 }
