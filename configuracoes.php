30ad3c717fa19e4d68bd3f114c91d5bd596a7ec6
Kaio Campos <kaiocamposti@gmail.com>
2020-07-01 18:40:17 -0300

    Mostrando as informções para serem editadas

--- configuracoes.php
+++ configuracoes.php
@@ -16,63 +16,68 @@ require_once("partials/menu.php");
 
 <section class="feed mt-10">
     <h1>Configurações</h1>
+    <?php if (!empty($_SESSION['flash'])) : ?>
+        <?= $_SESSION['flash']; ?>
+        <?php $_SESSION['flash'] = ''; ?>
+    <?php endif; ?>
 
     <form action="configuracoes_action.php" enctype="multipart/form-data" method="post" class="config-form">
-    <label>
-        Novo Avatar:
-        <input type="file" name="avatar"><br>
-        <img class="mini" src="<?=$base;?>/media/avatars/<?=$userInfo->avatar;?>"/>
-    </label>
-    <label>
-        Nova Capa:
-        <input type="file" name="cover"><br>
-        <img class="mini" src="<?=$base;?>/media/covers/<?=$userInfo->cover;?>"/>
-    </label>
-    <hr>
-    <label>
-        Nome Completo:<br>
-        <input type="text" name="name" value="<?=$userInfo->name;?>">
-    </label>
-    
-    <label>
-        Email:<br>
-        <input type="email" name="email" value="<?=$userInfo->email;?>">
-    </label>
+        <label>
+            Novo Avatar:
+            <input type="file" name="avatar"><br>
+            <img class="mini" src="<?= $base; ?>/media/avatars/<?= $userInfo->avatar; ?>" />
+        </label>
+        <label>
+            Nova Capa:
+            <input type="file" name="cover"><br>
+            <img class="mini" src="<?= $base; ?>/media/covers/<?= $userInfo->cover; ?>" />
+        </label>
+        <hr>
+        <label>
+            Nome Completo:<br>
+            <input type="text" name="name" value="<?= $userInfo->name; ?>">
+        </label>
 
-    <label>
-        Data de Nascimento:<br>
-        <input type="text" name="birthdate" id="birthdate" value="<?=date('d/m/Y', strtotime($userInfo->birthdate));?>">
-    </label>
+        <label>
+            Email:<br>
+            <input type="email" name="email" value="<?= $userInfo->email; ?>">
+        </label>
 
-    <label>
-        Cidade:<br>
-        <input type="text" name="city" value="<?=$userInfo->city;?>">
-    </label>
+        <label>
+            Data de Nascimento:<br>
+            <input type="text" name="birthdate" id="birthdate" value="<?= date('d/m/Y', strtotime($userInfo->birthdate)); ?>">
+        </label>
 
-    <label>
-        Trabalho:<br>
-        <input type="text" name="work" value="<?=$userInfo->work;?>">
-    </label>
-    <hr>
+        <label>
+            Cidade:<br>
+            <input type="text" name="city" value="<?= $userInfo->city; ?>">
+        </label>
 
-    <label>
-        Nova Senha:<br>
-        <input type="password" name="password">
-    </label>
-    <label>
-        Confirmar Nova Senha:<br>
-        <input type="password" name="password_confirmation">
-    </label>
+        <label>
+            Trabalho:<br>
+            <input type="text" name="work" value="<?= $userInfo->work; ?>">
+        </label>
+        <hr>
 
-    <button class="button">Salvar</button>
+        <label>
+            Nova Senha:<br>
+            <input type="password" name="password">
+        </label>
+        <label>
+            Confirmar Nova Senha:<br>
+            <input type="password" name="password_confirmation">
+        </label>
+
+        <button class="button">Salvar</button>
 
     </form>
 </section>
 <script src="https://unpkg.com/imask"></script>
-    <script>
-        IMask(
-            document.getElementById('birthdate'),
-            {mask:'00/00/0000'}
-        );
-    </script>
+<script>
+    IMask(
+        document.getElementById('birthdate'), {
+            mask: '00/00/0000'
+        }
+    );
+</script>
 <?php require_once("partials/footer.php"); ?>
