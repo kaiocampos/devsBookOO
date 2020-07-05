00236b13a5f0cba63942bab8fa454ae0598462be
Kaio Campos <kaiocamposti@gmail.com>
2020-07-05 06:55:50 -0300

    Implementando as funções de comments

--- dao/PostDaoMysql.php
+++ dao/PostDaoMysql.php
@@ -3,6 +3,7 @@ require_once("models/Post.php");
 require_once('dao/UserRelationDaoMysql.php');
 require_once('dao/UserDaoMysql.php');
 require_once('dao/PostLikeDaoMysql.php');
+require_once('dao/PostCommentDaoMysql.php');
 
 class PostDaoMysql implements PostDAO
 {
@@ -87,6 +88,7 @@ class PostDaoMysql implements PostDAO
         $posts = [];
         $userDao = new UserDaoMysql($this->pdo);
         $postLikeDao = new PostLikeDaoMysql($this->pdo);
+        $postCommentDao = new PostCommentDaoMysql($this->pdo);
 
         foreach ($post_list as $post_item) {
             $newPost = new Post();
@@ -108,7 +110,7 @@ class PostDaoMysql implements PostDAO
             $newPost->liked = $postLikeDao->isLiked($newPost->id, $id_user);
 
             // Informações sobre comments
-            $newPost->comments=[];
+            $newPost->comments = $postCommentDao->getComments($newPost->id);
 
             $posts[] = $newPost;
             
