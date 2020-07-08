a225876156397158268847474324a0d253cdab73
Kaio Campos <kaiocamposti@gmail.com>
2020-07-08 09:40:20 -0300

    Atualizando links e o script modal

--- partials/feed-item-script.php
+++ partials/feed-item-script.php
@@ -1,57 +1,83 @@
 <script>
-window.onload = function() {
-    //likes
-    document.querySelectorAll('.like-btn').forEach(item=>{
-        item.addEventListener('click', ()=>{
-            let id = item.closest('.feed-item').getAttribute('data-id');
-            let count = parseInt(item.innerText);
-            if(item.classList.contains('on') === false) {
-                item.classList.add('on');
-                item.innerText = ++count;
-            } else {
-                item.classList.remove('on');
-                item.innerText = --count;
-            }
-
-            fetch('ajax_like.php?id='+id);
-        });
-    });
-    //comentarios
-    document.querySelectorAll('.fic-item-field').forEach(item=>{
-        item.addEventListener('keyup', async (e)=>{
-            if(e.keyCode == 13) {
+    window.onload = function() {
+        //likes
+        document.querySelectorAll('.like-btn').forEach(item => {
+            item.addEventListener('click', () => {
                 let id = item.closest('.feed-item').getAttribute('data-id');
-                let txt = item.value;
-                item.value = '';
-
-                let data = new FormData();
-                data.append('id', id);
-                data.append('txt', txt);
-
-                let req = await fetch('ajax_comment.php', {
-                    method: 'POST',
-                    body: data
-                });
-                let json = await req.json();
-
-                if(json.error == '') {
-                    let html = '<div class="fic-item row m-height-10 m-width-20">';
-                    html += '<div class="fic-item-photo">';
-                    html += '<a href="'+json.link+'"><img src="'+json.avatar+'" /></a>';
-                    html += '</div>';
-                    html += '<div class="fic-item-info">';
-                    html += '<a href="'+json.link+'">'+json.name+'</a>';
-                    html += json.body;
-                    html += '</div>';
-                    html += '</div>';
-
-                    item.closest('.feed-item')
-                        .querySelector('.feed-item-comments-area')
-                        .innerHTML += html;
+                let count = parseInt(item.innerText);
+                if (item.classList.contains('on') === false) {
+                    item.classList.add('on');
+                    item.innerText = ++count;
+                } else {
+                    item.classList.remove('on');
+                    item.innerText = --count;
                 }
 
-            }
+                fetch('ajax_like.php?id=' + id);
+            });
         });
-    });
-};
+        //comentarios
+        document.querySelectorAll('.fic-item-field').forEach(item => {
+            item.addEventListener('keyup', async (e) => {
+                if (e.keyCode == 13) {
+                    let id = item.closest('.feed-item').getAttribute('data-id');
+                    let txt = item.value;
+                    item.value = '';
+
+                    let data = new FormData();
+                    data.append('id', id);
+                    data.append('txt', txt);
+
+                    let req = await fetch('ajax_comment.php', {
+                        method: 'POST',
+                        body: data
+                    });
+                    let json = await req.json();
+
+                    if (json.error == '') {
+                        let html = '<div class="fic-item row m-height-10 m-width-20">';
+                        html += '<div class="fic-item-photo">';
+                        html += '<a href="' + json.link + '"><img src="' + json.avatar + '" /></a>';
+                        html += '</div>';
+                        html += '<div class="fic-item-info">';
+                        html += '<a href="' + json.link + '">' + json.name + '</a>';
+                        html += json.body;
+                        html += '</div>';
+                        html += '</div>';
+
+                        item.closest('.feed-item')
+                            .querySelector('.feed-item-comments-area')
+                            .innerHTML += html;
+                    }
+
+                }
+            });
+        });
+
+        //upload of photos
+
+        // let feedPhoto = document.querySelector('.feed-new-photo');
+        // let feedFile = document.querySelector('.feed-new-file');
+
+        // feedPhoto.addEventListener('click', function() {
+        //     feedFile.click();
+        // });
+        // feedFile.addEventListener('change', async function() {
+        //     let photo = feedFile.files[0];
+        //     let formData = new FormData();
+
+        //     formData.append('photo', photo);
+        //     let req = await fetch('ajax_upload.php', {
+        //         method: 'POST',
+        //         body: formData
+        //     });
+        //     let json = await req.json();
+
+        //     if (json.error != '') {
+        //         alert(json.error);
+        //     }
+
+        //     window.location.href = window.location.href;
+        // });
+    };
 </script>
--- partials/feed-item.php
+++ partials/feed-item.php
@@ -1,58 +1,72 @@
-<?php 
+<?php
 require_once('partials/feed-item-script.php');
 $actionPhrase = '';
 switch ($item->type) {
     case 'text':
         $actionPhrase = 'Fez um post';
-    break;
-    
+        break;
+
     case 'photo':
-        $actionPhrase = 'Postou uma foto';        
-    break;
+        $actionPhrase = 'Postou uma foto';
+        break;
 }
 ?>
-<div class="box feed-item" data-id="<?=$item->id;?>">
+<div class="box feed-item" data-id="<?= $item->id; ?>">
     <div class="box-body">
         <div class="feed-item-head row mt-20 m-width-20">
             <div class="feed-item-head-photo">
-                <a href="<?=$base;?>/perfil.php?id=<?=$item->user->id;?>"><img src="<?=$base;?>/media/avatars/<?=$item->user->avatar;?>" /></a>
+                <a href="<?= $base; ?>/perfil.php?id=<?= $item->user->id; ?>"><img src="<?= $base; ?>/media/avatars/<?= $item->user->avatar; ?>" /></a>
             </div>
             <div class="feed-item-head-info">
-                <a href="<?=$base;?>/perfil.php?id=<?=$item->user->id;?>"><span class="fidi-name"><?=$item->user->name;?></span></a>
-                <span class="fidi-action"> <?=$actionPhrase;?> </span>
+                <a href="<?= $base; ?>/perfil.php?id=<?= $item->user->id; ?>"><span class="fidi-name"><?= $item->user->name; ?></span></a>
+                <span class="fidi-action"> <?= $actionPhrase; ?> </span>
                 <br />
-                <span class="fidi-date"><?=date('d/m/y', strtotime($item->created_at));?></span>
+                <span class="fidi-date"><?= date('d/m/y', strtotime($item->created_at)); ?></span>
             </div>
             <div class="feed-item-head-btn">
-                <img src="<?=$base;?>/assets/images/more.png" />
+                <img src="<?= $base; ?>/assets/images/more.png" />
             </div>
         </div>
         <div class="feed-item-body mt-10 m-width-20">
-            <?=nl2br($item->body);?>
+            <?php
+
+            switch ($item->type) {
+                case 'text':
+                    echo nl2br($item->body);
+                    break;
+
+                case 'photo':
+                    echo '<img src="{$base}/media/uploads/{$item->body}"/>';
+                    break;
+            }
+            ?>
+
+
+
         </div>
         <div class="feed-item-buttons row mt-20 m-width-20">
-            <div class="like-btn <?=($item->liked ? 'on':'');?>"><?=$item->likeCount;?></div>
-            <div class="msg-btn"><?=count($item->comments);?></div>
+            <div class="like-btn <?= ($item->liked ? 'on' : ''); ?>"><?= $item->likeCount; ?></div>
+            <div class="msg-btn"><?= count($item->comments); ?></div>
         </div>
         <div class="feed-item-comments">
             <div class="feed-item-comments-area">
-                <?php foreach($item->comments as $comment):?>
+                <?php foreach ($item->comments as $comment) : ?>
                     <div class="fic-item row m-height-10 m-width-20">
                         <div class="fic-item-photo">
-                            <a href="<?=$base;?>/perfil.php?id=<?=$comment->id_user;?>"><img src="<?=$base;?>/media/avatars/<?=$comment->user->avatar;?>" /></a>
+                            <a href="<?= $base; ?>/perfil.php?id=<?= $comment->id_user; ?>"><img src="<?= $base; ?>/media/avatars/<?= $comment->user->avatar; ?>" /></a>
                         </div>
                         <div class="fic-item-info">
-                            <a href="<?=$base;?>/perfil.php?id=<?=$comment->id_user;?>"><?=$comment->user->name;?></a>
-                            <?=$comment->body;?>
+                            <a href="<?= $base; ?>/perfil.php?id=<?= $comment->id_user; ?>"><?= $comment->user->name; ?></a>
+                            <?= $comment->body; ?>
                         </div>
                     </div>
 
-                <?php endforeach?>
+                <?php endforeach ?>
             </div>
-            
+
             <div class="fic-answer row m-height-10 m-width-20">
                 <div class="fic-item-photo">
-                    <a href="<?=$base;?>/perfil.php"><img src="<?=$base;?>/media/avatars/<?= $userInfo->avatar;?>" /></a>
+                    <a href="<?= $base; ?>/perfil.php"><img src="<?= $base; ?>/media/avatars/<?= $userInfo->avatar; ?>" /></a>
                 </div>
                 <input type="text" class="fic-item-field" placeholder="Escreva um comentário" />
             </div>
